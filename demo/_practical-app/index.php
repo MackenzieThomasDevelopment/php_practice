0f316b516f27c3f89791de5e58e1f6bce8096ec8
Mackenzie Thomas <mackenzie.thomas54@gmail.com>
2018-05-16 11:55:02 -0500

    Php practice
    copied over all of yesterdays work

---
+++ demo/_practical-app/index.php
@@ -0,0 +1,76 @@
+<?php include "functions.php"; ?>
+<?php include "includes/header.php";?>
+    
+  
+
+	<section class="content">
+
+		<aside class="col-sm-4">
+		
+		<?php Navigation();?>
+			
+		</aside><!--SIDEBAR-->
+
+		<article class="main-content col-sm-8">
+            
+            <h2 class='text-center'>PHP for beginners MINI Course</h2>
+
+			<h3>Please Read...........</h3>
+			
+                <p>I hope you made it this far and are able start practicing.With this little app you can practice a small part of PHP and hopefully remember some of the concepts with talked about.</p>
+
+                <p>I really enjoyed making this course for you and hopefully you appreciated it. Knowledge is a beautiful thing to have and time is precious, make every minute of your life count for something.
+                </p>
+
+                <p>Programming has changed my life and the minute I learned my first programming language I knew that was my way to success. I created PHP for beginners with everything needed to learn programming from scratch. After just a couple weeks my course became one of the most popular PHP courses in many online platforms. 
+
+                <p>On this mini course I will show you some Porgramming fundamentals along with PHP syntax and at the same time we will practive what you watch on the videos. This course is only 2.5 hours but it will show you enough to help you make a decision to go ALL IN.</p>
+
+                <p>After the mini course if you want to go ALL IN with the full course, click this link below for a HUGE discount <br><a target="_blank" href="http://codingfaculty.com/courses/php-for-complete-beginners-includes-msql-object-oriented?product_id=136732&coupon_code=PHP_YOUTUBE_DICOUNT_10"><strong>PHP for beginners Become a PHP MASTER</strong></a></p>
+
+
+                <p>Find all my courses at <br><a target="_blank" href="https://codingfaculty.com">CodingFaculty.com</a></p>
+
+
+                <p>If you want to ask me a question, <br>
+                 <a href="mailto:edwin@codingfaculty.com" target="_top">edwin@codingfaculty.com</a></p>
+
+
+                 <p><strong>PS:</strong> Please remember that I get thousands of emails and it might a take me a little while before responding to you, that is the only problem of becoming so popular :) but if you make a comment on <a href="http://CodingFaculty.com">CodingFaculty.com</a> (Which is free to sign up) in one of the courses, it's faster.</p>
+                 
+                 
+                 <article class="resources">
+                       
+                       
+                    <h3 class="text-center">Some Resource</h3>
+
+                    <a href="http://codingfaculty.com/courses/html-and-css-for-beginners-crash-course-learn-fast-easy" target="_blank" class="btn btn-primary btn-lg btn-block">FREE HTML and CSS COURSE</a>
+
+
+                    <a href="http://php.net/" target="_blank" class="btn btn-primary btn-lg btn-block">Php.net resource website</a>
+
+                    <a href="http://www.w3schools.com/php/" target="_blank" class="btn btn-primary btn-lg btn-block">Some PHP syntax references</a>
+                    
+            	 </article>
+                 
+                 
+                 
+                 
+                 
+    
+
+        </article><!--MAIN CONTENT-->
+
+
+
+
+
+	
+	
+
+<?php include "includes/footer.php"; ?>
+			
+			
+
+
+
---
+++ demo/variables.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+              
+<?php
+    
+$name = "Mackenzie";
+$number = 100;
+$last_Name = "Thomas";
+    
+echo $name;
+    
+echo $name . " " . $last_Name;
+    
+$hello = "<h1>HELLO</h1>";
+    
+echo $hello;
+    
+    
+    
+    
+?>
+</body>
+</html>
